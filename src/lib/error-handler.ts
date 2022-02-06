b7809458e220410b270766819013ac71435b50de
Roz <roz@rjmunhoz.me>
2022-02-06 15:52:37 -0300

    feat: cleaner error message
    Replaces the zod error message with a generic, cleaner one that tells you to look at the details
    property.

--- src/lib/error-handler.ts
+++ src/lib/error-handler.ts
@@ -1,13 +1,17 @@
 import { NextFunction, Request, Response } from 'express'
 import { ZodError } from 'zod'
 
-export const errorHandler = (err: any, _req: Request, res: Response, next: NextFunction) => {
+export const errorHandler = (
+  err: any,
+  _req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (err instanceof ZodError) {
-    return res.status(422)
-      .json({
-        message: err.message,
-        details: err.issues
-      })
+    return res.status(422).json({
+      message: 'Validation error. See `details` property',
+      details: err.issues
+    })
   }
 
   return next(err)
