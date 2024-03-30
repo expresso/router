5e73e6e7c098a23e71088637e8cc807aa4caad01
Lucas Santos <lhs.santoss@gmail.com>
2024-03-30 20:58:21 -0300

    fix(output): don't allow empty outputs
    Outputs will always have at least one response, requests cannot respond to nothing so now outputs do
    not allow empty responses
    
    BREAKING CHANGE: Any empty outputs will now fail type check. Any endpoints MUST now have at least
    one key in the output object

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,4 @@
 export type OneOrMore<T> = T | [T, ...T[]]
 export type ValueOf<T> = T[keyof T]
+export type NonEmptyObj<T extends Record<string, unknown>, Message extends string = 'Object cannot be empty'> =
+  T extends Record<string, never> ? Message : T
